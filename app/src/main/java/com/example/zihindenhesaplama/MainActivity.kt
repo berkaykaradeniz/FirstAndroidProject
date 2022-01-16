67ec7b411f4dea521f64d1d1012f2a562f4fc532
BerkayMertt <berkay_hakan_gs@hotmail.com>
2022-01-16 19:05:19 +0300

    Versiyon 2

--- app/src/main/java/com/example/zihindenhesaplama/MainActivity.kt
+++ app/src/main/java/com/example/zihindenhesaplama/MainActivity.kt
@@ -4,16 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val toplamabtn = findViewById<Button>(R.id.toplamabtn)
-        val kapat = findViewById<Button>(R.id.kapat)
 
         toplamabtn.setOnClickListener{
             val intent = Intent(this, Toplama::class.java)
@@ -24,6 +20,5 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
-        }
-
+    }
 }
--- app/src/main/java/com/example/zihindenhesaplama/Toplama.kt
+++ app/src/main/java/com/example/zihindenhesaplama/Toplama.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.os.CountDownTimer;
+import kotlinx.android.synthetic.main.activity_toplama.*
 
 
 class Toplama : AppCompatActivity() {
@@ -17,26 +18,11 @@ class Toplama : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toplama)
 
-        val geridon = findViewById<Button>(R.id.geridon)
-        val yeni_soru = findViewById<Button>(R.id.yeni_soru)
-        val islem = findViewById<TextView>(R.id.islem)
-        val cevap_1 = findViewById<TextView>(R.id.secenek1)
-        val cevap_2 = findViewById<TextView>(R.id.secenek2)
-        val seviye_artir = findViewById<TextView>(R.id.seviye_artir)
-        val seviye_düsür = findViewById<TextView>(R.id.seviye_düsür)
-        val seviye_artir_x10 = findViewById<TextView>(R.id.seviye_artir_x10)
-        val seviye_düsür_x10 = findViewById<TextView>(R.id.seviye_düsür_x10)
-        val seviye_text = findViewById<TextView>(R.id.seviye)
-        val secenek1 = findViewById<TextView>(R.id.secenek1)
-        val secenek2 = findViewById<TextView>(R.id.secenek2)
-
         var seviye = 1
         var yanlis_cevap = 0
         var dogru_cevap = 0
 
         seviye_text.text = "Seviye :" + seviye.toString()
-
-        yeni_soru.performClick()
 
         geridon.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
@@ -53,7 +39,7 @@ class Toplama : AppCompatActivity() {
             var sonuc = "?"
 
             dogru_cevap = a + b
-            yanlis_cevap = (1..20 * seviye).random()
+            yanlis_cevap = (1..10 * seviye).random()
 
             var dogru_cevap_konum = (0..1).random()
             islem.text = a.toString() + " + " + b.toString() + " = " + sonuc
@@ -62,11 +48,11 @@ class Toplama : AppCompatActivity() {
                 yanlis_cevap = (1..20 * seviye).random()
 
             if (dogru_cevap_konum == 1) {
-                cevap_1.text = dogru_cevap.toString()
-                cevap_2.text = yanlis_cevap.toString()
+                secenek1.text = dogru_cevap.toString()
+                secenek2.text = yanlis_cevap.toString()
             }else {
-                cevap_2.text = dogru_cevap.toString()
-                cevap_1.text = yanlis_cevap.toString()
+                secenek2.text = dogru_cevap.toString()
+                secenek1.text = yanlis_cevap.toString()
             }
 
         }
